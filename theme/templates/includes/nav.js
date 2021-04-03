a3571b13454764e39c46e25e9943e68631090dbc
sak96 <>
2021-04-03 11:21:27 +0530

    use localstorage instead of cookie for theming

--- theme/templates/includes/nav.js
+++ theme/templates/includes/nav.js
@@ -1,45 +1,27 @@
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == " ") {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + (";path=" + "{{SITEURL}}" + "/") + ";SameSite=Lax";
-}
 document.querySelector("#navbar-toggler").onclick = function (e) {
-    document.querySelectorAll(".nav-start a,.nav-end a").forEach(
-        (e) => e.classList.toggle("show")
-    )
-}
+  document
+    .querySelectorAll(".nav-start a,.nav-end a")
+    .forEach((e) => e.classList.toggle("show"));
+};
 {
-    let theme = getCookie("theme");
-    if (theme !== "light"){
-        document.documentElement.setAttribute('data-theme', ''); 
-        setCookie("theme", "", -1); 
-    }else{
-        document.documentElement.setAttribute('data-theme', theme);
-    }
+  let theme = localStorage.getItem("theme");
+  if (
+    theme === "light" ||
+    window.matchMedia("(prefers-color-scheme: light)").matches
+  ) {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  }
 }
 document.querySelector("#theme").onclick = function (e) {
-    if (document.documentElement.getAttribute('data-theme')){
-        document.documentElement.setAttribute('data-theme', ''); 
-        setCookie("theme", "", -1); 
-    }else{
-        let l = "light";
-        document.documentElement.setAttribute('data-theme', l); 
-        setCookie("theme", l, 365); 
-    }
-} 
+  if (document.documentElement.getAttribute("data-theme")) {
+    document.documentElement.setAttribute("data-theme", "");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+};
